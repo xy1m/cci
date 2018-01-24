6650f123070fa8ebbaa6cae3e070fcba6140c160
George Zhenpeng <gzhenpeng@Administrators-MacBook-Pro.local>
2018-01-24 14:30:19 -0800

    Add Bullet point to git commit message
    Should work
    
     - Apple
    
     - Banana

--- src/main/java/com/zzp/concurrent/blockingqueue/BlockingDequeExample.java
+++ src/main/java/com/zzp/concurrent/blockingqueue/BlockingDequeExample.java
@@ -3,5 +3,6 @@ package com.zzp.concurrent.blockingqueue;
 public class BlockingDequeExample {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("Bullet point should work");
     }
 }
