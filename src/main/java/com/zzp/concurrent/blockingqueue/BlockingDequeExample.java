92db403a684daeaf4b2ad7bf7ea0e32b590a9f45
George Zhenpeng <zhangzhpeng@gmail.com>
2018-06-08 14:10:02 -0700

    Move unrelated packages to other projects

--- src/main/java/com/zzp/concurrent/blockingqueue/BlockingDequeExample.java
+++
@@ -1,8 +0,0 @@
-package com.zzp.concurrent.blockingqueue;
-
-public class BlockingDequeExample {
-    public static void main(String[] args){
-        System.out.println("Hello World!");
-        System.out.println("Bullet point should work");
-    }
-}
--- src/main/java/com/zzp/funny/CountingMovie.java
+++
@@ -1,7 +0,0 @@
-package com.zzp.funny;
-
-public class CountingMovie {
-
-    public static void main(String[] args){
-    }
-}
