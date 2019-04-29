d21d78e53ae0f5cd689212235c2dd996d4f90f30
George Zhenpeng <zhangzhpeng@gmail.com>
2019-04-29 14:58:21 -0700

    Add SortedMerge and GroupAnagrams

--- src/main/java/com/xy1m/cci/chapter10_sorting_searching/Q10_01_Sorted_Merge.java
+++ src/main/java/com/xy1m/cci/chapter10_sorting_searching/Q10_01_Sorted_Merge.java
@@ -1,10 +1,42 @@
 package com.xy1m.cci.chapter10_sorting_searching;
 
-import java.util.LinkedList;
+import java.util.Arrays;
 
+/**
+ * Sorted Merge: You are given two sorted arrays, A and B, where A has a large enough buffer at the end to hold B.
+ * Write a method to merge B into A in sorted order.
+ */
 public class Q10_01_Sorted_Merge {
+
+    static void merge(int[] a, int[] b, int sizeA, int sizeB) {
+        int indexA = sizeA - 1;
+        int indexB = sizeB - 1;
+        int indexMerged = sizeA + sizeB - 1;
+        while (indexB >= 0) {
+            if (indexA >= 0 && a[indexA] > b[indexB]) {
+                a[indexMerged] = a[indexA--];
+            }
+            else {
+                a[indexMerged] = b[indexB--];
+            }
+            indexMerged--;
+        }
+    }
+
     public static void main(String[] args) {
-        LinkedList<Integer> list;
-        //TODO
+        int[] a = new int[10];
+        Arrays.fill(a, -1);
+        a[0] = 1;
+        a[1] = 3;
+        a[2] = 5;
+        a[3] = 7;
+        a[4] = 9;
+
+        int[] b = new int[]{2, 4, 6, 8};
+
+        merge(a, b, 5, 4);
+        for (int i : a) {
+            System.out.println(i);
+        }
     }
 }
--- src/main/java/com/xy1m/cci/chapter10_sorting_searching/Q10_02_Group_Anagrams.java
+++ src/main/java/com/xy1m/cci/chapter10_sorting_searching/Q10_02_Group_Anagrams.java
@@ -1,7 +1,47 @@
 package com.xy1m.cci.chapter10_sorting_searching;
 
+import com.xy1m.cci.lib.HashMapList;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Q10_02_Group_Anagrams {
+    static String sortChars(String s) {
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
+    }
+
+    static class AnagramComparator implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            return sortChars(o1).compareTo(sortChars(o2));
+        }
+    }
+
+    static void sort1(String[] array) {
+        Arrays.sort(array, new AnagramComparator());
+        System.out.println(Arrays.asList(array));
+    }
+
+    static void sort2(String[] array) {
+        HashMapList<String, String> mapList = new HashMapList<>();
+        for (String s : array) {
+            mapList.put(sortChars(s), s);
+        }
+        int index = 0;
+        for (String key : mapList.keySet()) {
+            List<String> list = mapList.get(key);
+            for (String t : list) {
+                array[index++] = t;
+            }
+        }
+        System.out.println(Arrays.asList(array));
+    }
+
     public static void main(String[] args) {
-        //TODO
+        sort1(new String[]{"abc", "aba", "cba", "acb"});
+        sort2(new String[]{"abc", "aba", "cba", "acb", "catdog", "docatg"});
     }
 }
